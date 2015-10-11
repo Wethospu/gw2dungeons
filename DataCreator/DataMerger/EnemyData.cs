ab414b45bac52d1feae92a33a18f464b65eeb7e8
Wethospu <wethospu@gmail.com>
2015-10-11 11:35:38 +0300

    Data: Added a new tool to merge collected data files. Relocated data files.

---
+++ DataCreator/DataMerger/EnemyData.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMerger
+{
+  class EnemyData
+  { 
+    [JsonProperty("occurrences")]
+    public Occurrence[] Occurrences { get; set; }
+  }
+
+  public class Occurrence : IComparable
+  {
+    public Occurrence()
+    {
+      MapID = -1;
+      Health = -1;
+      Level = -1;
+      ScaledLevel = -1;
+      Position = new Position();
+      Stamp = -1;
+    }
+
+    [JsonProperty("mapId")]
+    public int MapID { get; set; }
+
+    [JsonProperty("health")]
+    public int Health { get; set; }
+
+    [JsonProperty("level")]
+    public int Level { get; set; }
+
+    [JsonProperty("scaled_level")]
+    public int ScaledLevel { get; set; }
+
+    [JsonProperty("position")]
+    public Position Position { get; set; }
+
+    [JsonProperty("stamp")]
+    public int Stamp { get; set; }
+
+    public int CompareTo(object obj)
+    {
+      var toCompare = (Occurrence)(obj);
+      if (toCompare == null)
+        return 0;
+      if (MapID != toCompare.MapID)
+        return MapID > toCompare.MapID ? 1 : -1;
+      if (Level != toCompare.Level)
+        return Level > toCompare.Level ? 1 : -1;
+      if (ScaledLevel != toCompare.ScaledLevel)
+        return ScaledLevel > toCompare.ScaledLevel  ? 1 : -1;
+      if (Position == null && toCompare.Position != null)
+        return 1;
+      if (Position == null && toCompare.Position == null)
+        return 0;
+      if (Position != null && toCompare.Position == null)
+        return -1;
+      if (Position.CompareTo(toCompare.Position) != 0)
+        return Position.CompareTo(toCompare.Position);
+      if (Stamp != toCompare.Stamp)
+        return Stamp > toCompare.Stamp ? 1 : -1;
+      return 0;
+    }
+  }
+
+  public class Position : IComparable
+  {
+    public Position()
+    {
+      X = 0;
+      Y = 0;
+      Z = 0;
+    }
+
+    [JsonProperty("x")]
+    public double X { get; set; }
+
+    [JsonProperty("y")]
+    public double Y { get; set; }
+
+    [JsonProperty("z")]
+    public double Z { get; set; }
+
+    public int CompareTo(object obj)
+    {
+      var toCompare = (Position)(obj);
+      if (toCompare == null)
+        return 0;
+      if (X != toCompare.X)
+        return X > toCompare.X ? 1 : -1;
+      if (Y != toCompare.Y)
+        return Y > toCompare.Y ? 1 : -1;
+      if (Z != toCompare.Z)
+        return Z > toCompare.Z ? 1 : -1;
+      return 0;
+    }
+  }
+}
---
+++ DataCreator/DataMerger/Program.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataMerger
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
+      Dictionary<string, EnemyData> combined = new Dictionary<string, EnemyData>();
+
+      var dataFiles = Directory.GetFiles(".\\");
+      foreach (var file in dataFiles)
+      {
+        if (Path.GetExtension(file) != ".json" || Path.GetFileNameWithoutExtension(file).Equals("result"))
+          continue;
+        if (!File.Exists(file))
+        {
+          Console.WriteLine("File " + file + " doesn't exist!");
+          continue;
+        }
+        // Read the file. / 2015-10-11 / Wethospu
+        Dictionary<string, EnemyData> enemyData;
+        using (StreamReader r = new StreamReader(file))
+        {
+          string json = r.ReadToEnd();
+          enemyData = JsonConvert.DeserializeObject<Dictionary<string, EnemyData>>(json);
+        }
+        // Merge it. / 2015-10-11 / Wethospu
+        foreach (var key in enemyData.Keys)
+        {
+          if (combined.ContainsKey(key))
+          {
+            // Check which occurrences already exist. / 2015-10-11 / Wethospu
+            var occurrenceList = combined[key].Occurrences.ToList();
+            foreach (var occurrence in enemyData[key].Occurrences)
+            {
+              var match = false;
+              foreach (var occurrence2 in occurrenceList)
+              {
+                if (occurrence.MapID != occurrence2.MapID && occurrence.MapID > -1 && occurrence2.MapID > -1)
+                  continue;
+                if (occurrence.Health != occurrence2.Health && occurrence.Health > -1 && occurrence2.Health > -1)
+                  continue;
+                if (occurrence.Level != occurrence2.Level && occurrence.Level > -1 && occurrence2.Level > -1)
+                  continue;
+                if (occurrence.ScaledLevel != occurrence2.ScaledLevel && occurrence.ScaledLevel > -1 && occurrence2.ScaledLevel > -1)
+                  continue;
+                if (occurrence.Position != null && occurrence2.Position != null)
+                {
+                  if (occurrence.Position.X != occurrence2.Position.X)
+                    continue;
+                  if (occurrence.Position.Y != occurrence2.Position.Y)
+                    continue;
+                  if (occurrence.Position.Z != occurrence2.Position.Z)
+                    continue;
+                }
+                if (occurrence.Stamp != occurrence2.Stamp && occurrence.Stamp > -1 && occurrence2.Stamp > -1)
+                  continue;
+                match = true;
+                // Merge data. / 2015-10-11 / Wethospu
+                if (occurrence2.MapID < 0)
+                  occurrence2.MapID = occurrence.MapID;
+                if (occurrence2.Health < 0)
+                  occurrence2.Health = occurrence.Health;
+                if (occurrence2.Level < 0)
+                  occurrence2.Level = occurrence.Level;
+                if (occurrence2.ScaledLevel < 0)
+                  occurrence2.ScaledLevel = occurrence.ScaledLevel;
+                if (occurrence2.Stamp < 0)
+                  occurrence2.Stamp = occurrence.Stamp;
+                if (occurrence2.Position == null)
+                  occurrence2.Position = occurrence.Position;
+              }
+              if (!match)
+              {
+                occurrenceList.Add(occurrence);
+              }
+            }
+            occurrenceList.Sort();
+            combined[key].Occurrences = occurrenceList.ToArray();
+          }
+          else
+            combined.Add(key, enemyData[key]);
+        }
+      }
+      // Initialize all positions and check stuff. / 2015-10-11 / Wethospu
+      foreach (var value in combined.Values)
+      {
+        for (var i = 0; i < value.Occurrences.Length; i++)
+        {
+          if (value.Occurrences[i].Position == null)
+            value.Occurrences[i].Position = new Position();
+          if (value.Occurrences[i].Level > 1000)
+          {
+            // Remove corrupted data. / 2015-10-11 / Wethospu
+            var list = value.Occurrences.ToList();
+            list.RemoveAt(i);
+            value.Occurrences = list.ToArray();
+          }
+        }
+      }
+      // Save the result. / 2015-10-11 / Wethospu
+      using (StreamWriter r = new StreamWriter("result.json"))
+      {
+        var data = JsonConvert.SerializeObject(combined, Formatting.Indented);
+        r.Write(data);
+      }
+    }
+  }
+}
